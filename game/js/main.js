5ea0d1f07e589bdab231627fbbbba3febc38f11a
Thinh Nguyen <thinh@Thinhs-MacBook-Air.local>
2022-12-10 04:47:35 -0500

    second commit

---
+++ game/js/main.js
@@ -0,0 +1,598 @@
+"use strict";
+var barPoint = 50;
+var barPoint2 = 50;
+window.addEventListener('load', app);
+var mode = 0;
+var Player01 = 'O';
+var Player02 = 'X';
+var gameBoard = ['', '', '', '', '', '', '', '', ''];
+let winner = false;
+let gameOver = false;
+let turn = 0; // Keeps track if X or O player's turn
+
+
+const cells = document.querySelector('.board__main');
+
+
+const winningSequences = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6]
+];
+//SCOREBOARD
+const scoreboard = {
+  player1: 0,
+  player2: 0,
+  draw: 0,
+  newscore: 0,
+};
+// CREATE PLAYER
+const player = (name) => {
+  name = name;
+  return { name };
+};
+
+let playerX = player("");
+let playerY = player("");
+
+
+function check(input) {
+
+  var checkboxes = document.getElementsByClassName("radioCheck");
+
+
+  for (var i = 0; i < checkboxes.length; i++) {
+    //uncheck all
+    if (checkboxes[i].checked == true) {
+      checkboxes[i].checked = false;
+    }
+  }
+  //set checked of clicked object
+  if (input.checked == true) {
+    input.checked = false;
+  }
+  else {
+    input.checked = true;
+  }
+
+  let checkname = document.querySelectorAll('input[name="checkboxes"]:checked');
+  let output = [];
+  checkname.forEach((checkinput) => {
+    output.push(checkinput.value);
+  });
+  //Player Choice of X or O
+  if (output == 'O') {
+    Player02 = 'X';
+  } else {
+    Player02 = 'O';
+  }
+  Player01 = output;
+
+}
+
+function modePVP() {
+  // PVP Choice Button
+  const pvpFormContainer = document.querySelector('.landingpage');
+  const boardPVP = document.querySelector('.enter-players');
+  pvpFormContainer.classList.add('hide-container');
+  boardPVP.classList.remove('hide-container');
+
+
+
+}
+
+function modePVC() {
+
+
+  // PVC Choice Button
+  const pvpFormContainer = document.querySelector('.landingpage');
+  const boardPVC = document.querySelector('.enter-pvc');
+  pvpFormContainer.classList.add('hide-container');
+  boardPVC.classList.remove('hide-container');
+
+
+
+  let box = document.querySelector('.score').firstElementChild;
+
+  console.log(box);
+}
+
+
+// INITIALIZE APP
+function app() {
+  //focus();
+  // Calling PVP Button
+  const addPlayerForm = document.getElementById('player-form');
+  addPlayerForm.addEventListener('submit', addPlayers);
+
+  // calling PVC Button
+  const addPvCForm = document.getElementById('pvp-form');
+  addPvCForm.addEventListener('submit', addPvC);
+
+
+  let replayButton = document.querySelector('.replay-btn');
+  replayButton.addEventListener('click', resetBoard);
+
+
+  let backButton = document.querySelector('.back-btn');
+  backButton.addEventListener('click', backMode);
+
+}
+
+function backMode() {
+
+  location.reload();
+}
+
+// Add PLAYERS
+function addPlayers(event) {
+  event.preventDefault();
+
+  if (this.player1.value === '' || this.player2.value === '') {
+    alert('You Must Enter a Name for Each Field');
+    return;
+  }
+  const playerFormContainer = document.querySelector('.enter-players');
+  const boardMain = document.querySelector('.board__main');
+  playerFormContainer.classList.add('hide-container');
+  boardMain.classList.remove('hide-container');
+  // Player Name
+  playerX.name = this.player1.value;
+  playerY.name = this.player2.value;
+  mode = 1
+  buildBoard();
+
+}
+
+
+function addPvC(square) {
+  square.preventDefault();
+
+  if (this.player1.value === '') {
+    alert('You Must Enter a Name for Each Field');
+    return;
+  }
+
+  const pvcContainer = document.querySelector('.enter-pvc');
+  const boardPVC = document.querySelector('.board__main');
+  pvcContainer.classList.add('hide-container');
+  boardPVC.classList.remove('hide-container');
+
+  playerX.name = this.player1.value;
+  playerY.name = "Computer";
+  mode = 2
+
+  buildBoard();
+}
+
+// RETURN CURRENT PLAYER
+function currentPlayer() {
+
+  return turn % 2 === 0 ? Player02 : Player01;
+}
+
+// Resize squares in event browser is resized
+window.addEventListener("resize", onResize);
+function onResize() {
+  let allCells = document.querySelectorAll('.board__cell');
+  let cellHeight = allCells[0].offsetWidth;
+
+  allCells.forEach(cell => {
+    cell.style.height = `${cellHeight}px`;
+  });
+
+}
+
+// Build Board 
+function buildBoard() {
+
+  if (mode === 1) {
+    //Build board for PVP
+    let resetContainer =
+      document.querySelector('.reset');
+    resetContainer.classList.remove('reset--hidden');
+
+    addCellClickListener();
+
+
+  } else {
+    //Build Board For PVC
+    alert('mode PVC start')
+    let resetContainer = document.querySelector('.reset');
+    resetContainer.classList.remove('reset--hidden');
+
+
+    pvcCellClickListener();
+
+  }
+  onResize();
+  scoredBoard()
+  changeBoardHeaderNames();
+
+}
+
+// CELL CLICK EVENT FOR PLAYER TO ATTEMPT TO MAKE MOVE
+function makeMove(event) {
+
+  let currentCell = parseInt(event.currentTarget.firstElementChild.id);
+  let cellToAddToken = document.querySelector(`[id='${currentCell}']`);
+
+
+  if (cellToAddToken.innerHTML !== '') {
+    alert('This cell is already taken.');
+    return;
+  } else {
+    // Update turn count so next player can choose
+    turn++;
+
+    if (currentPlayer() === Player01) {
+      cellToAddToken.textContent = currentPlayer();
+      gameBoard[currentCell] = Player01;
+    } else {
+      cellToAddToken.textContent = currentPlayer();
+      gameBoard[currentCell] = Player02;
+    }
+  }
+  // CHECK IF WE HAVE A WINNER
+  isWinner();
+  // CHANGE BOARD HEADER INFO
+  changeBoardHeaderNames();
+  drawCheck();
+}
+
+function isWinner() {
+
+  // Check For Winner With Combo
+  winningSequences.forEach(winningCombos => {
+    let cell1 = winningCombos[0];
+    let cell2 = winningCombos[1];
+    let cell3 = winningCombos[2];
+    if (
+      gameBoard[cell1] === currentPlayer() &&
+      gameBoard[cell2] === currentPlayer() &&
+      gameBoard[cell3] === currentPlayer()
+    ) {
+      const cells = document.querySelectorAll('.board__cell');
+      cells.forEach(cell => {
+        let cellId = cell.firstElementChild.id;
+
+        if (cellId == cell1 || cellId == cell2 || cellId == cell3) {
+          cell.classList.add('board__cell--winner');
+        }
+      });
+
+      let currentPlayerText = document.querySelector('.board___player-turn');
+
+      if (currentPlayer() === Player01) {
+        currentPlayerText.innerHTML = `
+          <div class="congratulations">Congratulations ${playerX.name}</div>
+          <div class="u-r-winner">You are our winner!</div>
+        `;
+
+        barPoint = barPoint - 50 / 6;
+        console.log(barPoint + '%');
+        document.getElementById("score2").style.width = `${barPoint}%`;
+        winner = true;
+        scoreboard.player1++;
+        removeCellClickListener();
+        return true;
+      }
+
+      if (currentPlayer() === Player02) {
+        if (mode === 1) {
+          currentPlayerText.innerHTML = `
+          <div class="congratulations">Congratulations ${playerY.name}</div>
+          <div class="u-r-winner">You are our winner!</div>
+         `;
+        } else {
+          currentPlayerText.innerHTML = `
+          <div class="congratulations">It's the Computer Win</div>
+          <div class="u-r-winner">You Can't Win This Game!</div>
+         `;
+        }
+        barPoint2 = barPoint2 - 50 / 6;
+        console.log(barPoint2 + '%');
+        document.getElementById("score1").style.width = `${barPoint2}%`;
+        scoreboard.player2++;
+        winner = true;
+        removeCellClickListener();
+        return true;
+      }
+
+    }
+
+  });
+
+  scoredBoard();
+
+  return false;
+}
+
+function drawCheck() {
+  if (!winner) {
+    // Check if Draw Game for PVP
+    let currentPlayerText = document.querySelector('.board___player-turn');
+    console.log(turn)
+    if (turn === 9) {
+      currentPlayerText.innerHTML = `
+    <div class="congratulations">Game Is Tie</div>
+    <div class="u-r-winner">Resetting Board....!</div>
+  `;
+      scoreboard.draw++;
+      scoredBoard()
+      setTimeout(() => resetBoard(), 1000)
+    }
+  }
+
+}
+
+
+function scoredBoard() {
+  let currentPlayerText = document.querySelector('.board___player-turn');
+  // Insert Player Name and Scores to Score Board
+  if (scoreboard.player1 > 5 || scoreboard.player2 > 5) {
+    currentPlayerText.innerHTML = `
+      <span class="name--style">${playerX.name}</span>, won 6 games!
+      <div class="u-r-winner">Game Over! Reseting Score</div>
+    `
+    scoreboard.player1 = 0;
+    scoreboard.player2 = 0;
+    scoreboard.draw = 0;
+    scoreboard.newscore = 0;
+    barPoint = 50;
+    barPoint2 = 50;
+
+    document.getElementById("score1").style.width = '50%'
+    document.getElementById("score2").style.width = '50%'
+
+
+  }
+
+  score1.innerHTML = `
+    <p>${playerX.name}: ${scoreboard.player1}</p>
+    `;
+
+  score2.innerHTML = `
+    <p>${playerY.name}: ${scoreboard.player2}</p>
+    `;
+
+  draw.innerHTML = `
+    <p>Draw: ${scoreboard.draw}</p>
+    `;
+  newscore.innerHTML = `
+    <p>Game: ${scoreboard.player1 + scoreboard.player2 + scoreboard.draw}</p>
+    `;
+}
+
+
+function changeBoardHeaderNames() {
+  //Change Header Turn Function
+  if (!winner) {
+    let currentPlayerText = document.querySelector('.board___player-turn');
+    if (currentPlayer() === Player02) {
+      currentPlayerText.innerHTML = `
+        <span class="name--style">${playerX.name}</span>, you are up!
+        <div class="u-r-winner"></div>
+      `
+    } else {
+      currentPlayerText.innerHTML = `
+        <span class="name--style">${playerY.name}</span>, you are up.
+        <div class="u-r-winner"></div>
+      `
+    }
+  }
+}
+
+
+
+
+function resetBoard() {
+
+  // Reset Board
+
+  gameBoard = ['', '', '', '', '', '', '', '', ''];
+  turn = 0;
+  winner = false;
+
+  let currentPlayerText = document.querySelector('.board___player-turn');
+  currentPlayerText.innerHTML = `
+    <span class="name--style">${playerX.name}</span>, you are up!
+    <div class="u-r-winner"></div>
+  `
+  let cellToAddToken = document.querySelectorAll('.letter');
+  cellToAddToken.forEach(square => {
+    square.textContent = '';
+    square.parentElement.classList.remove('board__cell--winner');
+  });
+
+
+
+  if (mode === 1) {
+    //Reset Board for PVP
+    addCellClickListener();
+
+  } else {
+    (mode === 2)
+    //Reset Board for PVC
+    pvcCellClickListener();
+
+  }
+}
+
+
+function addCellClickListener() {
+  // PVP Click Listener
+  const cells = document.querySelectorAll('.board__cell');
+  cells.forEach(cell => {
+    cell.addEventListener('click', makeMove);
+  });
+}
+function removeCellClickListener() {
+
+  // Remove Player Click Action for Pvp
+  let allCells = document.querySelectorAll('.board__cell');
+  if (mode == 1) {
+
+    allCells.forEach(cell => {
+      cell.removeEventListener('click', makeMove);
+    });
+  }
+  else {
+    // Remove  Click Action for PVC
+    allCells.forEach(cell => {
+      cell.removeEventListener('click', turnClick);
+    });
+  }
+
+}
+
+// Computer Plays
+
+function pvcCellClickListener() {
+
+  //Player Vs computer CLick Listener
+
+  const cells = document.querySelectorAll('.board__cell');
+  gameBoard = Array.from(Array(9).keys());
+
+  cells.forEach(cell => {
+    cell.addEventListener('click', turnClick);
+  });
+
+
+}
+
+function turnClick(square) {
+  //Tracking Player Clicks
+  let currentCell = parseInt(square.currentTarget.firstElementChild.id);
+  let currentPlayerText = document.querySelector('.board___player-turn');
+
+  if (typeof gameBoard[currentCell] == 'number') {
+    turns(currentCell, Player01)
+
+    currentPlayerText.innerHTML = `<div class="congratulations">Hold on....!!!</div>
+     <div class="u-r-winner"> It's Computer Turn</div>`
+
+    if (!pvcTie() && !isWinner()) setTimeout(() => turns(bestSpot(), Player02), 200);
+  }
+}
+
+
+function turns(squareId, player) {
+
+  let currentPlayerText = document.querySelector('.board___player-turn');
+
+  currentPlayerText.innerHTML = `
+  <span class="name--style">${playerX.name}</span>, your turn!
+  <div class="u-r-winner"></div>`
+  // Computer Action Start
+  gameBoard[squareId] = player;
+  document.getElementById(squareId).innerText = player;
+
+  isWinner();
+
+}
+
+
+function pvcTie() {
+  let currentPlayerText = document.querySelector('.board___player-turn');
+  if (emptySquares().length == 0) {
+
+    setTimeout(() => resetBoard(), 1000)
+    scoreboard.draw++;
+    scoredBoard();
+    currentPlayerText.innerHTML = `<div class="congratulations">Game Is Tie</div>
+      <div class="u-r-winner">Resetting Board....!</div>
+    `;
+    return true;
+  }
+  return false;
+}
+
+
+function calculateWin(board, player) {
+
+  // Computer Calculation of Win & loss Scenerio
+  let plays = board.reduce((a, e, i) =>
+    (e === player) ? a.concat(i) : a, []);
+  let gameWon = null;
+  for (let [index, win] of winningSequences.entries()) {
+    if (win.every(elem => plays.indexOf(elem) > -1)) {
+      gameWon = { index: index, player: player };
+      break;
+    }
+  }
+  //  console.log(gameWon)
+  return gameWon;
+}
+
+function emptySquares() {
+  // Check For Empty Spaces
+  return gameBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+  // Returning Result of miniMax Algorithm
+  return minimax(gameBoard, Player02).index;
+}
+
+function minimax(newBoard, player) {
+
+  /// MiniMax algorithm AI Calculates Best Move By Go Through All Win Loses Scenerio
+  var availSpots = emptySquares();
+  //console.log(newBoard)
+
+  if (calculateWin(newBoard, Player01)) {
+    return { score: -10 };
+  } else if (calculateWin(newBoard, Player02)) {
+    return { score: 10 };
+  } else if (availSpots.length === 0) {
+    return { score: 0 };
+  }
+  var moves = [];
+  for (var i = 0; i < availSpots.length; i++) {
+    var move = {};
+    move.index = newBoard[availSpots[i]];
+    newBoard[availSpots[i]] = player;
+
+    if (player == Player02) {
+      var result = minimax(newBoard, Player01);
+      move.score = result.score;
+    } else {
+      var result = minimax(newBoard, Player02);
+      move.score = result.score;
+    }
+
+    newBoard[availSpots[i]] = move.index;
+
+    moves.push(move);
+  }
+
+  var bestMove;
+  if (player === Player02) {
+    var bestScore = -10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score > bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  } else {
+    var bestScore = 10000;
+    for (var i = 0; i < moves.length; i++) {
+      if (moves[i].score < bestScore) {
+        bestScore = moves[i].score;
+        bestMove = i;
+      }
+    }
+  }
+
+  return moves[bestMove];
+}
+
+
---
+++ inventory/script/script.js
@@ -0,0 +1,159 @@
+"use strict";
+var total = 0;
+
+
+function edit_row(no) {
+    document.getElementById("edit_button" + no).style.display = "none";
+    document.getElementById("save_button" + no).style.display = "block";
+
+    var brand = document.getElementById("brand_row" + no);
+    var model = document.getElementById("model_row" + no);
+    var serial = document.getElementById("serial_row" + no);
+    var quantity = document.getElementById("quantity_row" + no);
+    var price = document.getElementById("price_row" + no);
+
+    var brand_data = brand.innerHTML;
+    var model_data = model.innerHTML;
+    var serial_data = serial.innerHTML;
+    var quantity_data = quantity.innerHTML;
+    var price_data = price.innerHTML;
+
+    brand.innerHTML = "<input type='text' id='brand_text" + no + "' value='" + brand_data + "'>";
+    model.innerHTML = "<input type='text' id='model_text" + no + "' value='" + model_data + "'>";
+    serial.innerHTML = "<input type='text' id='serial_text" + no + "' value='" + serial_data + "'>";
+    quantity.innerHTML = "<input type='number' id='quantity_text" + no + "' value='" + quantity_data + "'>";
+    price.innerHTML = "<input type='number' id='price_text" + no + "' value='" + price_data + " $'>";
+}
+
+function save_row(no) {
+    var brand_val = document.getElementById("brand_text" + no).value;
+    var model_val = document.getElementById("model_text" + no).value;
+    var serial_val = document.getElementById("serial_text" + no).value;
+    var quantity_val = document.getElementById("quantity_text" + no).value;
+    var price_val = document.getElementById("price_text" + no).value;
+
+    // check blank space
+    if (brand_val == "" || model_val == "" || serial_val == "" || quantity_val == "" || price_val == "") {
+        alert("Please do not leave any blank space")
+        return
+    }
+
+    document.getElementById("brand_row" + no).innerHTML = brand_val;
+    document.getElementById("model_row" + no).innerHTML = model_val;
+    document.getElementById("serial_row" + no).innerHTML = serial_val;
+    document.getElementById("quantity_row" + no).innerHTML = quantity_val;
+    document.getElementById("price_row" + no).innerHTML = price_val;
+
+    document.getElementById("edit_button" + no).style.display = "block";
+    document.getElementById("save_button" + no).style.display = "none";
+    console.log(quantity_val);
+    totalStocks();
+    if (total > 100){
+        alert("Total Quantities should not be more than 100");
+        total = 0;
+        totalStocks();
+        edit_row(no);
+    }
+}
+
+
+function delete_row(no) {
+    document.getElementById("row" + no + "").outerHTML = "";
+    totalStocks()
+}
+
+function add_row() //Call function checkPassword
+{
+    if (checkPassword() == 1) {
+        var new_brand = document.getElementById("new_brand").value;
+        var new_model = document.getElementById("new_model").value;
+        var new_serial = document.getElementById("new_serial").value;
+        var new_quantity = document.getElementById("new_quantity").value;
+        var new_price = document.getElementById("new_price").value;
+
+        //check blank space
+        if (new_brand == "" || new_model == "" || new_serial == "" || new_quantity == "" || new_price == "") {
+            alert("Please do not leave any blank space")
+            return
+        }
+
+        var table = document.getElementById("data_table");
+        var table_len = (table.rows.length) - 1;
+        var row = table.insertRow(table_len).outerHTML = "<tr id='row" + table_len + "'><td id='brand_row" + table_len + "'>" + new_brand + "</td><td id='model_row" + table_len + "'>" + new_model + "</td><td id='serial_row" + table_len + "'>" + new_serial + "</td><td id='quantity_row" + table_len + "'>" + new_quantity + "</td><td id='price_row" + table_len + "'>" + new_price + " $ </td><td><input type='button' id='edit_button" + table_len + "' value='Edit' class='edit' onclick='edit_row(" + table_len + ")'> <input type='button' id='save_button" + table_len + "' value='Save' class='save' onclick='save_row(" + table_len + ")'> <input type='button' value='Delete' class='delete' onclick='delete_row(" + table_len + ")'></td></tr>";
+
+        
+
+    } else {
+        return
+    }
+
+    totalStocks();
+    console.log(total)
+    if (total > 100) {    
+       
+        alert("Total Quantities should not be more than 100");  
+        totalStocks();
+        delete_row(table_len);
+        total = 0;
+    }else{
+        
+        document.getElementById("new_brand").value = "";
+        document.getElementById("new_model").value = "";
+        document.getElementById("new_serial").value = "";
+        document.getElementById("new_quantity").value = "";
+        document.getElementById("new_price").value = "";
+    }
+}
+
+
+
+function startManage() {
+
+    const startFormContainer = document.querySelector('.welcome');
+    startFormContainer.classList.add('hide-container');
+    const inventoryMain = document.querySelector('.main-inventory');
+    inventoryMain.classList.remove('hide-container');
+    totalStocks();
+
+}
+
+var login_attempts = 0;
+function checkPassword() {
+    var checkpass = prompt("Enter your password to edit the inventory");
+    if (checkpass == 'password') { // check pass
+        return 1
+    } else { // if in correct then attempt again       
+        login_attempts = login_attempts + 1;
+        alert("Login Failed. You have tried (" + login_attempts + "/3) Login Attempts");
+
+        if (login_attempts >= 3) {
+            alert("No Login Attempts Available");
+
+            location.reload();
+            return
+        }
+    }
+}
+
+
+
+function totalStocks() {
+    var table=document.getElementById("data_table");
+    var table_len=(table.rows.length)-1;
+    var sum = 0;
+
+    for (let i = 1; i < table_len; i++) {
+       var s = document.getElementById("quantity_row" + i).innerHTML;
+       sum = parseInt(s) + sum;
+    }
+
+    if (sum > 100) {
+       
+        total = sum;
+    }else{
+        document.getElementById("totalQuantity").value = Number.parseInt(sum);
+    }
+
+}
+
+
